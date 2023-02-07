8fb83a73d886841842a42d91fdb687422ee4a193
NYeonK <skdus728@gmail.com>
2023-02-07 21:02:21 +0900

    [ feat ] atoms 폴더 및 기본 button 생성 + storybook test

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Meta, Story } from "@storybook/react";
+import Button, { ButtonProps } from ".";
+
+export default {
+  title: "Components/atoms/Button",
+  component: Button,
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = { text: "우리는 최고다!" };
